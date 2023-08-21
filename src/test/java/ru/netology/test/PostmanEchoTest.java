3099cd9d3a9180d768d4f977fdce63bfa5d54e54
EvgeniyS <xjamsex@gmail.com>
2023-08-21 16:52:03 +0300

    Correct code

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("word"));
+                .body("data", equalTo("some data"));
     }
 }
 

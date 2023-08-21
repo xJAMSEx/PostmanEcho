94d57fcb3faee3a3a43854e398ffff2edf61110b
EvgeniyS <xjamsex@gmail.com>
2023-08-21 16:48:24 +0300

    Mistake commit

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("word"));
     }
 }
 

1b1df683b814d3dc56df198a6be7fd30eaf2eef5
EvgeniyS <xjamsex@gmail.com>
2023-08-21 16:43:11 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
+
+
+
